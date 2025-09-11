284671ea7f3b0250b5a0a4e8e92de4e68a6f5d0e
Shin <phanhoangnam17@gmail.com>
2025-09-11 09:20:45 +0700

    Update Role

--- user-service/src/main/java/com/example/userservice/enums/EnumRole.java
+++ user-service/src/main/java/com/example/userservice/enums/EnumRole.java
@@ -3,10 +3,8 @@ package com.example.userservice.enums;
 public enum EnumRole {
     ADMIN ,
     CUSTOMER ,
-    WAREHOUSE_MANAGER ,
-    SALE,
-    ORDER_MANAGER,
-    DRIVER ,
-    WAREHOUSE_STAFF ,
-    HR,
+    SELLER ,
+    DELIVERER,
+    BRANCH_MANAGER,
+
 }
