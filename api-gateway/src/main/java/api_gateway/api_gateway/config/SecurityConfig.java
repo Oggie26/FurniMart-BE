88fdaf873ed6a5f37c8628378bb92858f6c5905b
Shin <phanhoangnam17@gmail.com>
2025-09-22 14:09:56 +0700

    fix gateway continue

--- api-gateway/src/main/java/api_gateway/api_gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/api_gateway/api_gateway/config/SecurityConfig.java
@@ -4,33 +4,35 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 @EnableWebFluxSecurity
 @EnableReactiveMethodSecurity
 public class SecurityConfig {
 
-        @Bean
-        public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
-            return http
-                    .csrf(ServerHttpSecurity.CsrfSpec::disable)
-                    .cors()
-                    .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
-                    .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
-                    .authorizeExchange(exchanges -> exchanges
-                            .pathMatchers(
-                                    "/swagger-ui.html",
-                                    "/swagger-ui/**",
-                                    "/v3/api-docs/**",
-                                    "/webjars/**",
-                                    "/api/auth/**",
-                                    "/api/products/**",
-                                    "/notification-service/**"
-                            ).permitAll()
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .cors(cors -> {})
+                .httpBasic(httpBasic -> httpBasic.disable())
+                .formLogin(form -> form.disable())
+                .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers(
+                                "/swagger-ui.html",
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**",
+                                "/webjars/**",
+                                "/api/auth/**",
+                                "/api/products/**",
+                                "/notification-service/**"
+                        ).permitAll()
+                        .anyExchange().authenticated()
+                );
 
         return http.build();
     }
 }
-
