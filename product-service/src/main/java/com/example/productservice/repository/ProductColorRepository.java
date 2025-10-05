088e0f041b96f83fbdc5990e484548ca2f4e6107
Shin <phanhoangnam17@gmail.com>
2025-10-05 11:03:12 +0700

    Fix Product Service to build docker

--- product-service/src/main/java/com/example/productservice/repository/ProductColorRepository.java
+++ product-service/src/main/java/com/example/productservice/repository/ProductColorRepository.java
@@ -3,10 +3,11 @@ package com.example.productservice.repository;
 import com.example.productservice.entity.ProductColor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ProductColorRepository extends JpaRepository<ProductColor, String> {
     Optional<ProductColor> findByIdAndIsDeletedFalse(String id);
-    ProductColor findByProductIdAndIsDeletedFalse(String id);
+    List<ProductColor> findByProductIdAndIsDeletedFalse(String id);
     boolean existsByProductIdAndColorId(String productId, String colorId);
 }
