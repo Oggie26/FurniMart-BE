f4c6ed5d3d27523430c592b8e8da8dc24a96d837
Shin <phanhoangnam17@gmail.com>
2025-10-05 09:53:04 +0700

    Fix Product Service to build docker

--- product-service/src/main/java/com/example/productservice/response/ProductResponse.java
+++ product-service/src/main/java/com/example/productservice/response/ProductResponse.java
@@ -25,5 +25,6 @@ public class ProductResponse {
     private String categoryName;
     private Long categoryId;
     private EnumStatus status;
+    private List<ProductColor> productColors;
     private List<MaterialResponse> materials;
 }
--- product-service/src/main/java/com/example/productservice/service/ProductServiceImpl.java
+++ product-service/src/main/java/com/example/productservice/service/ProductServiceImpl.java
@@ -179,6 +179,7 @@ public class ProductServiceImpl implements ProductService {
                 .thumbnailImage(product.getThumbnailImage())
                 .width(product.getWidth())
                 .height(product.getHeight())
+                .productColors(product.getProductColors())
                 .status(product.getStatus())
                 .length(product.getLength())
                 .weight(product.getWeight())
