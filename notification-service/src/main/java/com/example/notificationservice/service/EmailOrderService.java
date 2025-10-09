58d3358e2335ca06fc05d019903a73203f843cf0
Shin <phanhoangnam17@gmail.com>
2025-10-10 00:21:04 +0700

    FIX Kafka Notificaiton service

--- notification-service/src/main/java/com/example/notificationservice/service/EmailOrderService.java
+++ notification-service/src/main/java/com/example/notificationservice/service/EmailOrderService.java
@@ -2,9 +2,11 @@ package com.example.notificationservice.service;
 
 import com.example.notificationservice.event.OrderCreatedEvent;
 import com.example.notificationservice.exception.AppException;
+import com.example.notificationservice.feign.AuthClient;
 import com.example.notificationservice.feign.OrderClient;
 import com.example.notificationservice.feign.UserClient;
 import com.example.notificationservice.response.ApiResponse;
+import com.example.notificationservice.response.AuthResponse;
 import com.example.notificationservice.response.OrderResponse;
 import com.example.notificationservice.response.UserResponse;
 import com.example.notificationservice.enums.ErrorCode;
@@ -14,6 +16,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
@@ -27,17 +31,17 @@ public class EmailOrderService {
     private final TemplateEngine templateEngine;
     private final UserClient userClient;
     private final OrderClient orderClient;
+    private final AuthClient authClient;
 
     public void sendMailToCreateOrderSuccess(OrderCreatedEvent event) {
         try {
             String link = "http://localhost:5173/orders/" + event.getOrderId();
             String button = "Xem chi tiết đơn hàng";
 
-            UserResponse user = getUser(event.getUserId());
             OrderResponse order = getOrder(event.getOrderId());
 
             Context context = new Context();
-            context.setVariable("name", user.getFullName());
+            context.setVariable("name", getUserId());
             context.setVariable("button", button);
             context.setVariable("link", link);
             context.setVariable("orderDate", order.getOrderDate());
@@ -51,12 +55,12 @@ public class EmailOrderService {
             MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
 
             helper.setFrom("namphse173452@fpt.edu.vn", "FurniMart");
-            helper.setTo(user.getEmail());
+            helper.setTo(getUserId());
             helper.setSubject("🛒 Đơn hàng #" + order.getId() + " của bạn đã được thanh toán thành công!");
             helper.setText(htmlContent, true);
 
             mailSender.send(mimeMessage);
-            log.info("✅ Email đơn hàng gửi thành công tới {}", user.getEmail());
+            log.info("✅ Email đơn hàng gửi thành công tới {}", getUserId());
 
         } catch (MessagingException e) {
             log.error("❌ Lỗi khi gửi email: {}", e.getMessage());
@@ -67,12 +71,24 @@ public class EmailOrderService {
         }
     }
 
-    private UserResponse getUser(String id) {
-        ApiResponse<UserResponse> userResp = userClient.getUserById(id);
-        if (userResp == null || userResp.getData() == null) {
+    private String getUserId() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !authentication.isAuthenticated()
+                || "anonymousUser".equals(authentication.getPrincipal())) {
+            throw new AppException(ErrorCode.UNAUTHENTICATED);
+        }
+
+        String username = authentication.getName();
+        ApiResponse<AuthResponse> response = authClient.getUserByUsername(username);
+
+        if (response == null || response.getData() == null) {
             throw new AppException(ErrorCode.NOT_FOUND_USER);
         }
-        return userResp.getData();
+        ApiResponse<UserResponse> userId = userClient.getUserByAccountId(response.getData().getId());
+        if (userId == null || userId.getData() == null) {
+            throw new AppException(ErrorCode.NOT_FOUND_USER);
+        }
+        return userId.getData().getEmail();
     }
 
     private OrderResponse getOrder(Long id) {
