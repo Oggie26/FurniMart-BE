14417c00fbedad80fe20e1b54deeafc00de521cc
Shin <phanhoangnam17@gmail.com>
2025-11-21 13:04:05 +0700

    fix ai-service

--- inventory-service/src/main/java/com/example/inventoryservice/entity/Inventory.java
+++ inventory-service/src/main/java/com/example/inventoryservice/entity/Inventory.java
@@ -7,6 +7,7 @@ import lombok.*;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -47,8 +48,8 @@ public class Inventory extends AbstractEntity {
     @JoinColumn(name = "warehouse_id")
     private Warehouse warehouse;
 
-    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<InventoryItem> inventoryItems;
+    @OneToMany(mappedBy = "inventory", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<InventoryItem> inventoryItems = new ArrayList<>();
 
     @PrePersist
     public void prePersist() {
