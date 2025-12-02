3e43c5e0ea43133914174e960598deb173cd5bcb
Shin <phanhoangnam17@gmail.com>
2025-12-02 07:09:45 +0700

    fix ai-service

--- inventory-service/src/main/java/com/example/inventoryservice/config/CloudinaryConfig.java
+++ inventory-service/src/main/java/com/example/inventoryservice/config/CloudinaryConfig.java
@@ -18,6 +18,7 @@ public class CloudinaryConfig {
     @Value("${cloudinary.api-secret}")
     private String apiSecret;
 
+
     @Bean
     public Cloudinary cloudinary() {
         return new Cloudinary(ObjectUtils.asMap(
