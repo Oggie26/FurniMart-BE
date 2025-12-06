ef91539f055b0ee36722857cc70b008e178f1f87
Shin <phanhoangnam17@gmail.com>
2025-12-06 09:34:04 +0700

    add Template mail

--- notification-service/src/main/java/com/example/notificationservice/listener/OrderEventListener.java
+++ notification-service/src/main/java/com/example/notificationservice/listener/OrderEventListener.java
@@ -1,6 +1,13 @@
 package com.example.notificationservice.listener;
 
+import com.example.notificationservice.enums.EnumProcessOrder;
+import com.example.notificationservice.enums.ErrorCode;
+import com.example.notificationservice.enums.PaymentMethod;
 import com.example.notificationservice.event.OrderCreatedEvent;
+import com.example.notificationservice.exception.AppException;
+import com.example.notificationservice.feign.OrderClient;
+import com.example.notificationservice.response.ApiResponse;
+import com.example.notificationservice.response.OrderResponse;
 import com.example.notificationservice.service.EmailOrderService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,17 +18,46 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @RequiredArgsConstructor
 public class OrderEventListener {
-    private final EmailOrderService orderService;
 
-    @KafkaListener(topics = "order-created-topic",
+    private final EmailOrderService orderService;
+    private final OrderClient orderClient;
+
+    @KafkaListener(
+            topics = "order-created-topic",
             groupId = "notification-group",
             containerFactory = "orderCreatedKafkaListenerContainerFactory"
     )
     public void handleOrderCreated(OrderCreatedEvent event) {
-        log.info("Received OrderCreatedEvent for order: {}", event.getOrderId());
-        orderService.sendMailToCreateOrderSuccess(event);
+        log.info("📦 Received OrderCreatedEvent for order: {}", event.getOrderId());
+
+        OrderResponse order = getOrderResponse(event.getOrderId());
+
+        if (order == null) {
+            log.warn("Order with ID {} not found. Skipping notification.", event.getOrderId());
+            return;
+        }
+        if (order.getPayment().getPaymentMethod().equals(PaymentMethod.COD) && order.getStatus().equals(EnumProcessOrder.MANAGER_ACCEPT)){
+            orderService.sendMailToCreateOrderSuccess(event);
+        }
+        orderService.sendMailToManagerAcceptedOrder(event);
         for (OrderCreatedEvent.OrderItem item : event.getItems()) {
             String key = "reserved_stock:" + item.getProductColorId();
+            log.info("Reserved stock key: {}", key);
+        }
+    }
+
+    private OrderResponse getOrderResponse(long orderId) {
+        try {
+            ApiResponse<OrderResponse> response = orderClient.getOrderById(orderId);
+            if (response != null && response.getData() != null) {
+                return response.getData();
+            } else {
+                log.warn("OrderClient returned null for orderId {}", orderId);
+                return null;
+            }
+        } catch (Exception e) {
+            log.error("Error fetching order with id {}: {}", orderId, e.getMessage(), e);
+            throw new AppException(ErrorCode.NOT_FOUND_ORDER);
         }
     }
 }
--- notification-service/src/main/java/com/example/notificationservice/service/EmailOrderService.java
+++ notification-service/src/main/java/com/example/notificationservice/service/EmailOrderService.java
@@ -1,23 +1,15 @@
 package com.example.notificationservice.service;
 
 import com.example.notificationservice.event.OrderCreatedEvent;
-import com.example.notificationservice.exception.AppException;
 import com.example.notificationservice.feign.AuthClient;
 import com.example.notificationservice.feign.OrderClient;
 import com.example.notificationservice.feign.UserClient;
-import com.example.notificationservice.response.ApiResponse;
-import com.example.notificationservice.response.AuthResponse;
-import com.example.notificationservice.response.OrderResponse;
-import com.example.notificationservice.response.UserResponse;
-import com.example.notificationservice.enums.ErrorCode;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
@@ -35,7 +27,7 @@ public class EmailOrderService {
 
     public void sendMailToCreateOrderSuccess(OrderCreatedEvent event) {
         try {
-            String link = "http://localhost:5173/orders/" + event.getOrderId();
+            String link = "https://furnimart-web.vercel.app/orders/";
             String button = "Xem chi tiết đơn hàng";
 
 
@@ -69,4 +61,39 @@ public class EmailOrderService {
         }
     }
 
+    public void sendMailToManagerAcceptedOrder(OrderCreatedEvent event){
+        try {
+            String link = "https://furnimart-web.vercel.app/orders/";
+            String button = "Xem chi tiết đơn hàng";
+
+            Context context = new Context();
+            context.setVariable("name", event.getFullName());
+            context.setVariable("button", button);
+            context.setVariable("link", link + event.getOrderId());
+            context.setVariable("orderDate", event.getOrderDate());
+            context.setVariable("paymentMethod", event.getPaymentMethod());
+            context.setVariable("totalAmount", event.getTotalPrice());
+            context.setVariable("items", event.getItems());
+
+            String htmlContent = templateEngine.process("orderAcceptedByManager", context);
+
+            MimeMessage mimeMessage = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
+
+            helper.setFrom("namphse173452@fpt.edu.vn", "FurniMart");
+            helper.setTo(event.getEmail());
+            helper.setSubject("Đơn hàng #" + event.getOrderId() + " đã được Manager chấp nhận!");
+            helper.setText(htmlContent, true);
+
+            mailSender.send(mimeMessage);
+            log.info("Email thông báo Manager accept đơn hàng gửi thành công tới {}", event.getEmail());
+
+        } catch (MessagingException e) {
+            log.error("Lỗi khi gửi email: {}", e.getMessage());
+            throw new RuntimeException("Lỗi khi gửi email: " + e.getMessage());
+        } catch (Exception ex) {
+            log.error("Lỗi xử lý dữ liệu email: {}", ex.getMessage());
+            throw new RuntimeException(ex);
+        }
+    }
 }
