b9de7ddc1546ee47d0aa2ad6d1498e43a6b836e1
Shin <phanhoangnam17@gmail.com>
2025-12-09 10:01:12 +0700

    fix ai service

--- ai-service/src/main/java/com/example/aiservice/controller/AnalyzeController.java
+++ ai-service/src/main/java/com/example/aiservice/controller/AnalyzeController.java
@@ -5,6 +5,7 @@ import com.example.aiservice.response.InteriorDesignResponse;
 import com.example.aiservice.service.AiInteriorDesignServiceImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,12 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RequestMapping("/api/ai/analyze")
-@RequiredArgsConstructor
 @Slf4j
 @RestController
 public class AnalyzeController {
 
-    private final AiInteriorDesignServiceImpl aiInteriorDesignService;
+    @Autowired
+    private AiInteriorDesignServiceImpl aiInteriorDesignService;
 
     @PostMapping(value = "/analyze-room", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<ApiResponse<InteriorDesignResponse>> analyzeRoom(
