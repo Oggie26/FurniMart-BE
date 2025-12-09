342de499b2f535e4c4069df8185edcfb42f5f194
Shin <phanhoangnam17@gmail.com>
2025-12-09 09:28:46 +0700

    fix ai service

--- ai-service/src/main/java/com/example/aiservice/controller/ChatBoxController.java
+++ ai-service/src/main/java/com/example/aiservice/controller/ChatBoxController.java
@@ -1,5 +1,6 @@
 package com.example.aiservice.controller;
 
+
 import com.example.aiservice.response.ApiResponse;
 import com.example.aiservice.response.InteriorDesignResponse;
 import com.example.aiservice.service.AiInteriorDesignService;
@@ -8,10 +9,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-@RestController
 @RequestMapping("/api/ai")
 @RequiredArgsConstructor
 @Slf4j
